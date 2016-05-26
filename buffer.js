4a0752b5bd2fe53a555e14f1d364c3b8e6c86e29
Claire Liu <wenjing.liu09@foxmail.com>
2016-05-26 11:15:04 +0800

    Buffer practice

---
+++ buffer.js
@@ -0,0 +1,24 @@
+let bin = new Buffer([0x68, 0x65, 0x6c, 0x6c, 0x6f]);
+let dup = new Buffer(bin.length);
+
+bin.copy(dup);
+dup[0] = 0x48;
+console.log(bin);
+console.log(dup);
+
+
+// const bin = new Buffer([0x68, 0x65, 0x6c, 0x6c, 0x6f]);
+
+// console.log(bin[0]);
+// console.log(bin.length);
+
+// const str = bin.toString('utf-8');
+// console.log(str);
+
+// let bin2 = new Buffer('hello','utf-8');
+// console.log(bin2);
+
+// let sub = bin.slice(2);
+// console.log(bin);
+// sub[0] = 0x65;
+// console.log(bin);
