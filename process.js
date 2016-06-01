d90cda6c5aaf52f89e575220e210ca78d812e32b
Claire Liu <wenjing.liu09@foxmail.com>
2016-06-01 14:16:19 +0800

    example --version 1.0

--- process.js
+++ process.js
@@ -1,6 +1,17 @@
-function main(argv){
+/*function main(argv){
   console.log(argv);
 }
 
 console.log(process.argv);
-main(process.argv.slice(2));
+main(process.argv.slice(2));*/
+
+
+function test(){
+  try{
+    throw(new Error('err'));
+  }catch(err){
+    process.exit(1);
+  }
+}
+
+
